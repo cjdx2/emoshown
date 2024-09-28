6326a5c1c3f4c108e6fbf0e6de36f6531b7199e4
cjdx2 <102336000+cjdx2@users.noreply.github.com>
2024-09-28 13:34:33 +0800

    homie

--- EmoShown/JS/HomeScreen.js
+++ EmoShown/JS/HomeScreen.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Modal, Pressable, Image } from 'react-native';
-import { signOut } from 'firebase/auth';  
-import { auth } from './firebaseConfig'; 
+import { signOut } from 'firebase/auth';
+import { auth } from './firebaseConfig';
+import quotes from './quotes.json';
 
 export function HomeScreen({ navigation }) {
-  const username = "Username"; 
   const [currentDate, setCurrentDate] = useState('');
   const [modalVisible, setModalVisible] = useState(false);
+  const [quote, setQuote] = useState('');
+  const [username, setUsername] = useState('');
 
   const handleLogout = () => {
     signOut(auth)
@@ -36,27 +38,43 @@ export function HomeScreen({ navigation }) {
   }, []);
 
   useEffect(() => {
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    setQuote(randomQuote);
+  }, []);
+
+  useEffect(() => {
+    const user = auth.currentUser;
+    if (user) {
+      setUsername(user.displayName || '');
+    }
+  }, []);
+
+  useEffect(() => {
     navigation.setOptions({
       headerLeft: () => null, 
       headerRight: () => (
         <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.menuButton}>
-        <Image source={require('../assets/menu.png')} style={styles.menuButtonImage} />
-      </TouchableOpacity>
+          <Image source={require('../assets/menu.png')} style={styles.menuButtonImage} />
+        </TouchableOpacity>
       ),
     });
   }, [navigation]);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.date}>{currentDate}</Text>
-      <Text style={styles.title}>Welcome to EmoShown!</Text>
-      <View style={styles.quoteContainer}>
-        <Text style={styles.quote}>
-          "When I talk to myself as I would a friend, I see all my best qualities and I allow myself to shine."
-        </Text>
+      <View style={styles.headerContainer}>
+        <View style={styles.dateContainer}>
+          <Text style={styles.date}>{currentDate}</Text>
+        </View>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Welcome, {username || 'Guest'}!</Text>
+        </View>
       </View>
-
-      {/* Bottom Navigation Icons */}
+  
+      <View style={styles.quoteContainer}>
+        <Text style={styles.quote}>{quote}</Text>
+      </View>
+  
       <View style={styles.bottomNav}>
         <TouchableOpacity style={styles.iconButton} onPress={() => navigation.navigate('MoodJournal')}>
           <Image source={require('../assets/dailymood.png')} style={styles.icon} />
@@ -74,7 +92,7 @@ export function HomeScreen({ navigation }) {
           <Image source={require('../assets/community.png')} style={styles.icon} />
         </TouchableOpacity>
       </View>
-
+  
       <Modal
         animationType="slide"
         transparent={true}
@@ -82,6 +100,7 @@ export function HomeScreen({ navigation }) {
         onRequestClose={() => setModalVisible(false)}>
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
+            <Text style={styles.date}>{currentDate}</Text>
             <Pressable onPress={handleLogout} style={styles.modalButton}>
               <Text style={styles.modalButtonText}>Logout</Text>
             </Pressable>
@@ -94,23 +113,34 @@ export function HomeScreen({ navigation }) {
     </View>
   );
 }
-
+  
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start', // Changed to flex-start
     alignItems: 'center',
     backgroundColor: '#fff',
     padding: 20,
   },
+  headerContainer: {
+    alignSelf: 'flex-start',
+    marginBottom: 20,
+    width: '100%', // Ensures it takes full width
+  },
+  dateContainer: {
+    alignSelf: 'flex-start',
+    marginBottom: 5,
+  },
   date: {
     fontSize: 18,
-    marginBottom: 10,
+    textAlign: 'left', // Aligns text to the left
+  },
+  titleContainer: {
+    alignSelf: 'flex-start',
   },
   title: {
     fontSize: 28,
     fontWeight: 'bold',
-    marginBottom: 20,
   },
   quoteContainer: {
     backgroundColor: '#f0f0f0',
@@ -119,26 +149,10 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     borderColor: '#000',
     borderWidth: 1,
-    borderRadius: 8,
+    alignItems: 'center', // Center the quote text
   },
   quote: {
-    fontSize: 16,
-    fontStyle: 'italic',
-    textAlign: 'center',
-  },
-  menuButton: {
-    marginRight: 10,
-    padding: 10,
-    backgroundColor: '#fff',
-    borderColor: '#000',
-    borderWidth: 1,
-    borderRadius: 8,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  menuButtonImage: {
-    width: 24,
-    height: 24, 
+    textAlign: 'center', // Center the text within the quote container
   },
   bottomNav: {
     position: 'absolute',
@@ -157,20 +171,30 @@ const styles = StyleSheet.create({
   },
   icon: {
     width: 30,
-    height: 30, // Adjust this size depending on your icon size
+    height: 30,
   },
   modalContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
     backgroundColor: 'rgba(0,0,0,0.5)',
+    padding: 20,
   },
   modalContent: {
-    width: 300,
+    width: 250,
+    maxHeight: 150,
     padding: 20,
     backgroundColor: '#fff',
     borderRadius: 10,
     alignItems: 'center',
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    position: 'absolute',
+    top: 20,
+    left: 20,
   },
   modalButton: {
     padding: 10,
--- EmoShown/JS/IntroductionScreen.js
+++ EmoShown/JS/IntroductionScreen.js
@@ -1,9 +1,8 @@
-// IntroductionScreen.js
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Image, ImageBackground } from 'react-native';
 
 export function IntroductionScreen({ navigation }) {
-  const [currentSlide, setCurrentSlide] = useState(-1); // Start with -1 to display the intro first
+  const [currentSlide, setCurrentSlide] = useState(-1);
 
   const slides = [
     { title: "Monitor Your Emotions", image: require('../assets/onboard1.png') },
@@ -13,28 +12,42 @@ export function IntroductionScreen({ navigation }) {
 
   const handleNext = () => {
     if (currentSlide === -1) {
-      // First click, move to the first slide
       setCurrentSlide(0);
     } else if (currentSlide < slides.length - 1) {
-      // Move through slides
       setCurrentSlide(currentSlide + 1);
     } else {
-      // After the last slide, navigate to login
       navigation.navigate('Login');
     }
+  };
+
+  const renderProgressBar = () => {
+    return (
+      <View style={styles.progressContainer}>
+        {slides.map((_, index) => (
+          <View
+            key={index}
+            style={[
+              styles.progressDot,
+              { backgroundColor: index <= currentSlide ? '#333333' : '#ababab' },
+            ]}
+          />
+        ))}
+      </View>
+    );
   };
 
   return (
     <View style={styles.container}>
       {currentSlide === -1 ? (
-        // Initial Introduction Screen content
         <>
           <Text style={styles.title}>Welcome to</Text>
-          <Text style={styles.appName}>EmoShown</Text>
+          <Text style={styles.appName}>
+            <Text style={styles.emoText}>Emo</Text>
+            <Text style={styles.shownText}>Shown</Text>
+          </Text>
           <Text style={styles.subtitle}>Your Emotional Wellness Hub</Text>
         </>
       ) : (
-        // Slide content with background image
         <ImageBackground
           source={slides[currentSlide].image}
           style={styles.backgroundImage}
@@ -47,6 +60,8 @@ export function IntroductionScreen({ navigation }) {
       <TouchableOpacity style={styles.nextButton} onPress={handleNext}>
         <Image source={require('../assets/rightarrow.png')} style={styles.icon} />
       </TouchableOpacity>
+
+      {renderProgressBar()}
     </View>
   );
 }
@@ -59,22 +74,36 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    color: '#000', // White text for better visibility on the background
-    textAlign: 'center',
-    marginBottom: 20,
-  },
-  appName: {
     fontSize: 40,
     fontWeight: 'bold',
-    color: '#4ECDC4', // EmoShown color styling
+    color: '#000',
+    textAlign: 'left',
+    alignSelf: 'flex-start',
+    marginLeft: 20,
+    marginTop: -150,
+  },
+  appName: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: '#000',
+    textAlign: 'left',
+    alignSelf: 'flex-start',
+    marginLeft: 20,
+    marginTop: -10,
+  },
+  emoText: {
+    color: '#a3c2e8',
+  },
+  shownText: {
+    color: '#b9dabf',
   },
   subtitle: {
-    fontSize: 24,
-    textAlign: 'center',
-    marginVertical: 20,
-    color: '#333',
+    fontSize: 35,
+    color: '#000',
+    textAlign: 'left',
+    alignSelf: 'flex-start',
+    marginLeft: 20,
+    marginTop: 50,
   },
   backgroundImage: {
     flex: 1,
@@ -90,12 +119,23 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
   },
   icon: {
-    width: 40,  // Set your preferred size
-    height: 40, // Set your preferred size
+    width: 60,
+    height: 60,
     borderColor: '#000',
     borderWidth: 1,
     borderRadius: 8,
+    backgroundColor: 'white',
+  },
+progressContainer: {
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 20,
+    alignItems: 'center',
+  },
+  progressDot: {
+    width: 8,
+    height: 8,
+    borderRadius: 5,
+    marginHorizontal: 5,
   },
 });
-
-
